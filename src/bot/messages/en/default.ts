b472e372477610c42da31c7af17100aab8c32952
Niranjan Rajendran <me@niranjan.io>
2017-06-25 16:36:48 +0530

    Add messages

--- src/bot/messages/en/default.ts
+++ src/bot/messages/en/default.ts
@@ -1,11 +1,11 @@
-const stripIndent = require('common-tags/lib/stripIndent');
+import * as stripIndent from 'common-tags/lib/stripIndent';
 
 export default {
-    greeting(username) {
-        return `Hi @${username}!`
-    },
-    needsInfo() {
-        return stripIndent`
+  greeting(username: string): string {
+    return `Hi @${username}!`;
+  },
+  needsInfo(): string {
+    return stripIndent`
             Looks like you're missing some information we'll need to replicate this issue.
             
             The easier it is for us to decipher an issue with the info provided,
@@ -17,59 +17,59 @@ export default {
             2. The behavior you expect
             3. The browser & OS you use
             4. A [short, self-contained example](http://sscce.org/)
-        `
-    },
-    unlinkedPullRequest() {
-        return stripIndent`
+        `;
+  },
+  unlinkedPullRequest(): string {
+    return stripIndent`
             Looks like you haven't linked this to any issue. 
             It would be great if you could follow the best practices and link your PR with the correct issue :)
-        `
-    },
-    pullRequestWithoutDeploymentLink() {
-        return stripIndent`
+        `;
+  },
+  pullRequestWithoutDeploymentLink(): string {
+    return stripIndent`
             I'm unable to find a deployment link for this PR. 
             Did you forget to add one ? ;)
-        `
-    },
-    buildFailed() {
-        return stripIndent`
+        `;
+  },
+  buildFailed(): string {
+    return stripIndent`
             It looks like one or more of your builds have failed.
             I\'ve added the relevant info below to save you some time.\n
-        `
-    },
-    buildFailureItem(buildNumber, owner, repo, id) {
-        return stripIndent`
+        `;
+  },
+  buildFailureItem(buildNumber: string, owner: string, repo: string, id: string): string {
+    return stripIndent`
             - [Build ${buildNumber}](https://travis-ci.org/${owner}/${repo}/jobs/${id}) has \`failed\`
-        `
-    },
-    movedIssueBody(user, originalIssueUrl, originalIssueBody) {
-        return stripIndent`
+        `;
+  },
+  movedIssueBody(user, originalIssueUrl, originalIssueBody): string {
+    return stripIndent`
             ###### Original issue submitted by @${user} in ${originalIssueUrl}\n---\n${originalIssueBody}
-        `
-    },
-    issueMovedComment(newIssueUrl) {
-        return stripIndent`
+        `;
+  },
+  issueMovedComment(newIssueUrl: string): string {
+    return stripIndent`
             I've [moved your issue](${newIssueUrl}) to the correct repository.
             Please make sure to keep an eye on the new issue for the latest information.
-        `
-    },
-    closedWithoutMerging() {
-        return stripIndent`
+        `;
+  },
+  closedWithoutMerging(): string {
+    return stripIndent`
             Looks like this PR was closed without merging. 
             Please try not to make test/temporary PRs :) Keeps the repo clean.
-        `
-    },
-    prHasConflicts() {
-        return stripIndent`
+        `;
+  },
+  prHasConflicts(): string {
+    return stripIndent`
             Looks like your PR has some conflicts. :worried:
             Could you resolve them and rebase on top of the latest upstream code ?
-        `
-    },
-    prCannotBeRebased() {
-        return stripIndent`
+        `;
+  },
+  prCannotBeRebased(): string {
+    return stripIndent`
             Looks like your PR is behind the upstream
             Could you rebase it on top of the latest upstream code ?
-        `
-    }
+        `;
+  }
 
 };
---
+++ src/bot/messages/index.ts
@@ -0,0 +1,2 @@
+import * as messages from './en/default';
+export default messages;
